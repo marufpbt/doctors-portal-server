8625bac4c23a35a23529909768a17a79050d499d
jhankarpHero <61257321+jhankarpHero@users.noreply.github.com>
2020-10-11 15:34:08 -0500

    server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs-extra');
 const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
@@ -60,18 +61,32 @@ client.connect(err => {
 
     app.post('/addADoctor', (req, res) => {
         const file = req.files.file;
-        
         const name = req.body.name;
         const email = req.body.email;
-        console.log(name, email, file);
-        file.mv(`${__dirname}/doctors/${file.name}`, err => {
+        const filePath = `${__dirname}/doctors/${file.name}`;
+        file.mv(filePath, err => {
             if(err){
                 console.log(err);
-                return res.status(500).send({msg: 'Failed to upload Image'});
+                res.status(500).send({msg: 'Failed to upload Image'});
             }
-            doctorCollection.insertOne({name, email, img:file.name})
+            const newImg = fs.readFileSync(filePath);
+            const encImg = newImg.toString('base64');
+
+            var image = {
+                  contentType: req.files.file.mimetype,
+                  size: req.files.file.size,
+                  img: Buffer(encImg, 'base64')
+               };
+
+            doctorCollection.insertOne({name, email, image})
             .then(result => {
-                res.send(result.insertedCount > 0)
+                fs.remove(filePath, error => {
+                    if(error) {
+                        console.log(error);
+                        res.status(500).send({msg: 'Failed to upload Image'});
+                    }
+                    res.send(result.insertedCount > 0);
+                })
             })
             // return res.send({name: file.name, path: `/${file.name}`})
         })
