ba84d69227ab1825a0a932dfe50a389614712524
Maruf <66161467+marufpbt@users.noreply.github.com>
2021-04-13 19:51:27 +0600

    read problem fixed

--- index.js
+++ index.js
@@ -1,98 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const MongoClient = require('mongodb').MongoClient;
+const express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+const { ObjectId } = require('mongodb')
 require('dotenv').config()
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-
+const port = process.env.PORT || 5000
 const app = express()
-
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('doctors'));
-app.use(fileUpload());
 
-const port = 5000;
-
-app.get('/', (req, res) => {
-    res.send("hello from db it's working working")
-})
-
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.edbhc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const appointmentCollection = client.db("doctors-portal").collection("appointment");
-    const doctorCollection = client.db("doctors-portal").collection("doctors");
 
-    app.post('/addAppointment', (req, res) => {
-        const appointment = req.body;
-        appointmentCollection.insertOne(appointment)
-            .then(result => {
-                res.send(result.insertedCount > 0)
-            })
-    });
 
-    app.get('/appointments', (req, res) => {
-        appointmentCollection.find({})
-            .toArray((err, documents) => {
-                res.send(documents);
-            })
-    })
+	const appointmentCollection = client.db("doctors-portal").collection("appointment");
+	//Create
+	// app.post('/addProduct', (req, res) => {
+	// 	const product = req.body;
+	// 	productCollection.insertOne(product)
+	// 		.then(result => {
+	// 			console.log("data added succesfully");
+	// 			res.redirect('/')
+	// 		})
+	// })
+	//Read
+	app.get('/appointments', (req, res) => {
+		appointmentCollection.find({})
+			.toArray((err, documents) => {
+				res.send(documents);
+			})
+	})
+	//Read
+	// app.get('/product/:id', (req, res) => {
+	// 	appointmentCollection.find({ _id: ObjectId(req.params.id) })
+	// 		.toArray((err, documents) => {
+	// 			res.send(documents[0])
+	// 		})
+	// })
+	//delete
+	// app.delete('/delete/:id', (req, res) => {
+	// 	appointmentCollection.deleteOne({ _id: ObjectId(req.params.id) })
+	// 		.then(result => {
+	// 			res.send(result.deletedCount > 0)
+	// 		})
+	// })
+	//patch
+	// app.patch('/update/:id', (req, res) => {
+	// 	appointmentCollection.updateOne({ _id: ObjectId(req.params.id) },
+	// 		{
+	// 			$set: { price: req.body.price, quantity: req.body.quantity }
+	// 		}
+	// 	)
+	// 		.then(result => {
+	// 			res.send(result.modifiedCount > 0)
+	// 		})
 
-    app.post('/appointmentsByDate', (req, res) => {
-        const date = req.body;
-        const email = req.body.email;
-        doctorCollection.find({ email: email })
-            .toArray((err, doctors) => {
-                const filter = { date: date.date }
-                if (doctors.length === 0) {
-                    filter.email = email;
-                }
-                appointmentCollection.find(filter)
-                    .toArray((err, documents) => {
-                        console.log(email, date.date, doctors, documents)
-                        res.send(documents);
-                    })
-            })
-    })
+	// })
+	//end CRUD
 
-    app.post('/addADoctor', (req, res) => {
-        const file = req.files.file;
-        const name = req.body.name;
-        const email = req.body.email;
-        const newImg = file.data;
-        const encImg = newImg.toString('base64');
-
-        var image = {
-            contentType: file.mimetype,
-            size: file.size,
-            img: Buffer.from(encImg, 'base64')
-        };
-
-        doctorCollection.insertOne({ name, email, image })
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-    })
-
-    app.get('/doctors', (req, res) => {
-        doctorCollection.find({})
-            .toArray((err, documents) => {
-                res.send(documents);
-            })
-    });
-
-    app.post('/isDoctor', (req, res) => {
-        const email = req.body.email;
-        doctorCollection.find({ email: email })
-            .toArray((err, doctors) => {
-                res.send(doctors.length > 0);
-            })
-    })
 
 });
 
+app.listen(port)
 
-app.listen(process.env.PORT || port)
+// npm install dotenv express mongodb body-parser cors nodemon
+//create .env file
+//DB_USER=marufpbt
+// DB_PASS=MarufCU97259725
+// DB_NAME=doctors-portal
